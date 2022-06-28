93cb60d74a1de590a51f518a9101b538f0861019
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-06-28 11:28:34 -0300

    url

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [ProductModule,
-    MongooseModule.forRoot(`mongodb+srv://localhost:Y3Bu5RvKc0fDddLj@cluster0.j3uwp.mongodb.net/nest?retryWrites=true&w=majority`, {
+    MongooseModule.forRoot(`${process.env.MONGOURL}`, {
       useNewUrlParser: true
     })],
   controllers: [AppController],
